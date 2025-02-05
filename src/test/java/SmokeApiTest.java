10fbc166f96b8fae9748a238b80e48423ce9a05e
MaximSinyavsky <razarsinet1991@gmail.com>
2025-02-05 23:17:30 +0200

    update project

--- src/test/java/SmokeApiTest.java
+++ src/test/java/SmokeApiTest.java
@@ -1,5 +1,27 @@
+import static io.restassured.RestAssured.given;
+
 public class SmokeApiTest {
+    private static final String BASE_URL = "https://petstore.swagger.io/v2/";
     void createUserTest(){
-        
+        //given - config
+        //when - http
+        //then - assert
+        String bodyJson = """
+                {
+                "id": 0,
+                  "username": "string",
+                  "firstName": "string",
+                  "lastName": "string",
+                  "email": "string",
+                  "password": "string",
+                  "phone": "string",
+                  "userStatus": 0
+                  }""";
+        given()
+                .header("accept", "application/json")
+                .header("content-type", "application/json")
+                .body(bodyJson)
+                .when().post(BASE_URL+ "user")
+                .then();
     }
 }
