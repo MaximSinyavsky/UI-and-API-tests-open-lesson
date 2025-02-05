ff21304e7fb637f2ceffde83ee13ce996599a024
MaximSinyavsky <razarsinet1991@gmail.com>
2025-02-06 00:04:10 +0200

    update project

--- src/test/java/SmokeApiTest.java
+++ src/test/java/SmokeApiTest.java
@@ -1,9 +1,16 @@
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class SmokeApiTest {
     private static final String BASE_URL = "https://petstore.swagger.io/v2/";
+
+    @Test
     void createUserTest(){
         //given - config
         //when - http
@@ -27,5 +34,11 @@ public class SmokeApiTest {
                 .then();
         response.statusCode(200);
         response.extract().response().prettyPrint();
+        response.body("code", Matchers.equalTo(200));
+        response.body("type", Matchers.equalTo("unknown"));
+        response.body("message", Matchers.notNullValue(String.class));
+        Response responseAsResponse = response.extract().response();
+        responseAsResponse.prettyPrint();
+        Assertions.assertEquals(200, responseAsResponse.statusCode());
     }
 }
