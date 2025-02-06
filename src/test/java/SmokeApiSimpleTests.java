a915b11798615e94a5cdea88ed46bd8e68dd8c7d
MaximSinyavsky <razarsinet1991@gmail.com>
2025-02-06 15:23:34 +0200

    update project

--- src/test/java/SmokeApiSimpleTests.java
+++ src/test/java/SmokeApiSimpleTests.java
@@ -1,13 +1,12 @@
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class SmokeApiTest {
+public class SmokeApiSimpleTest {
     private static final String BASE_URL = "https://petstore.swagger.io/v2/";
 
     @Test
