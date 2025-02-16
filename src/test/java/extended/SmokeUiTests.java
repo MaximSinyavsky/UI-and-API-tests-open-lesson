e023d7061e88dd52ef8b9b94e83b590b6252b7bb
MaximSinyavsky <razarsinet1991@gmail.com>
2025-02-16 11:20:36 +0200

    update project

--- src/test/java/extended/SmokeUiTests.java
+++ src/test/java/extended/SmokeUiTests.java
@@ -1,4 +1,36 @@
 package extended;
 
+
+import extended.pages.HomePage;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@Feature("SmokeUiTests")
+@Tag("ui")
+@Tag("smoke")
 public class SmokeUiTests {
+    HomePage homePage;
+
+    @BeforeEach
+    void setUp(){
+        homePage = new HomePage(new ChromeDriver());
+    }
+
+    @AfterEach
+    void tearDown(){
+        homePage.quit();
+    }
+
+    @Test
+    @DisplayName("Check form submitted and has title")
+    void submitWebFormTest(){
+        webFormPage = webFormPage = homePage.openWebForm();
+
+        webFormPage.fillTextInpute("text");
+        webFormPage.submitForm();
+        webelement title = webFormePage.getTitle();
+
+        Assertions.assertEquals("Form submitted", tittle.getText());
+    }
 }
